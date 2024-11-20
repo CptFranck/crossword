88eff391ff135306af1afc56d65d06cd04637e28
CptFranck <francois.caillet71@gmail.com>
2024-11-20 11:21:01 +0100

    add randomDirection static function

--- include/Direction.h
+++ include/Direction.h
@@ -1,3 +1,7 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+#pragma once
+
 enum Direction
 {
     UP,
@@ -5,3 +9,19 @@ enum Direction
     RIGHT,
     LEFT
 };
+
+static std::random_device rd;
+static std::mt19937 gen(rd());
+static std::vector<Direction> ALL_DIRECTIONS({Direction::UP,
+                                              Direction::DOWN,
+                                              Direction::RIGHT,
+                                              Direction::LEFT});
+
+static Direction randomDirection()
+{
+    std::uniform_int_distribution<size_t> dis(0, ALL_DIRECTIONS.size() - 1);
+    Direction randomDirection = ALL_DIRECTIONS[dis(gen)];
+    return randomDirection;
+}
+
+#endif
