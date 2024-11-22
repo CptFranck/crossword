f9ad9e96cb2ca4bf8bb3532584ba6ae689fda572
CptFranck <francois.caillet71@gmail.com>
2024-11-22 20:08:17 +0100

    remove iostream use

--- src/WordDefinition.cpp
+++ src/WordDefinition.cpp
@@ -1,5 +1,4 @@
 #include "WordDefinition.h"
-#include "iostream"
 
 WordDefinition::WordDefinition(std::string word, std::string definition)
 {
@@ -13,17 +12,13 @@ WordDefinition::~WordDefinition()
 
 std::map<std::pair<char, size_t>, std::vector<size_t>> WordDefinition::findCommonLetterPosition(std::string futurWord) const
 {
-    // futur word letter and position : list of working positions in current crosswordLine
     std::map<std::pair<char, size_t>, std::vector<size_t>> potentialIntersection;
     int cIndex = 0;
-    std::cout << "futur " << futurWord << std::endl;
-    std::cout << "courant " << word << std::endl;
     for (const char &c : futurWord)
     {
         size_t pos = word.find(c, 0);
         while (pos != std::string::npos)
         {
-            std::cout << "lettre trouvée " << futurWord << " |" << c << "| en commun à " << pos << std::endl;
             std::pair<char, size_t> key(c, cIndex);
             potentialIntersection[key].push_back(pos);
             pos = word.find(c, pos + 1);
