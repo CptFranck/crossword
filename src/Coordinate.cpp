bb85509279bc8004ae5677689b6b399293e20b23
CptFranck <francois.caillet71@gmail.com>
2024-12-02 17:40:15 +0100

    remove unused functions

--- src/Coordinate.cpp
+++ src/Coordinate.cpp
@@ -11,27 +11,6 @@ Coordinate::~Coordinate()
 {
 }
 
-// Coordinate Coordinate::getOppositionFrom(Direction wordDirection, Direction d) const
-// {
-//     int futur_x = this->x;
-//     int futur_y = this->y;
-//     if (wordDirection == Direction::LEFT || wordDirection == Direction::RIGHT)
-//     {
-//         if (d == Direction::UP)
-//             futur_y += 1;
-//         else
-//             futur_y -= 1;
-//     }
-//     else
-//     {
-//         if (d == Direction::LEFT)
-//             futur_y += 1;
-//         else
-//             futur_y -= 1;
-//     }
-//     return Coordinate(futur_x, futur_y);
-// }
-
 Coordinate Coordinate::getPositionFrom(int position, Direction d) const
 {
     int futur_x = this->x;
@@ -73,11 +52,6 @@ void Coordinate::ifMaxUpdate(Coordinate c)
         this->x = c.x;
     if (c.y > this->y)
         this->y = c.y;
-}
-
-void Coordinate::print()
-{
-    std::cout << "x: " << this->x << " y: " << this->y << std::endl;
 }
 
 int Coordinate::getX() const
